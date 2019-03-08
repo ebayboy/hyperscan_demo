5d2bbf345d105abd5c298880e231b3aee9791a6c
fanpengfei <fanpengfei@jd.com>
2019-03-08 05:58:58 -0500

    fw

--- simplegrep_multi.c
+++ simplegrep_multi.c
@@ -13,144 +13,54 @@ static int on_match(unsigned int id, unsigned long long from,
     return 0;
 }
 
-/**
- * Fill a data buffer from the given filename, returning it and filling @a
- * length with its length. Returns NULL on failure.
- */
-static char *readInputData(const char *inputFN, unsigned int *length) {
-    FILE *f = fopen(inputFN, "rb");
-    if (!f) {
-        fprintf(stderr, "ERROR: unable to open file \"%s\": %s\n", inputFN,
-                strerror(errno));
-        return NULL;
-    }
-
-    /* We use fseek/ftell to get our data length, in order to keep this example
-     * code as portable as possible. */
-    if (fseek(f, 0, SEEK_END) != 0) {
-        fprintf(stderr, "ERROR: unable to seek file \"%s\": %s\n", inputFN,
-                strerror(errno));
-        fclose(f);
-        return NULL;
-    }
-    long dataLen = ftell(f);
-    if (dataLen < 0) {
-        fprintf(stderr, "ERROR: ftell() failed: %s\n", strerror(errno));
-        fclose(f);
-        return NULL;
-    }
-    if (fseek(f, 0, SEEK_SET) != 0) {
-        fprintf(stderr, "ERROR: unable to seek file \"%s\": %s\n", inputFN,
-                strerror(errno));
-        fclose(f);
-        return NULL;
-    }
-
-    /* Hyperscan's hs_scan function accepts length as an unsigned int, so we
-     * limit the size of our buffer appropriately. */
-    if ((unsigned long)dataLen > UINT_MAX) {
-        dataLen = UINT_MAX;
-        printf("WARNING: clipping data to %ld bytes\n", dataLen);
-    } else if (dataLen == 0) {
-        fprintf(stderr, "ERROR: input file \"%s\" is empty\n", inputFN);
-        fclose(f);
-        return NULL;
-    }
-
-    char *inputData = malloc(dataLen);
-    if (!inputData) {
-        fprintf(stderr, "ERROR: unable to malloc %ld bytes\n", dataLen);
-        fclose(f);
-        return NULL;
-    }
-
-    char *p = inputData;
-    size_t bytesLeft = dataLen;
-    while (bytesLeft) {
-        size_t bytesRead = fread(p, 1, bytesLeft, f);
-        bytesLeft -= bytesRead;
-        p += bytesRead;
-        if (ferror(f) != 0) {
-            fprintf(stderr, "ERROR: fread() failed\n");
-            free(inputData);
-            fclose(f);
-            return NULL;
-        }
-    }
-
-    fclose(f);
-
-    *length = (unsigned int)dataLen;
-    return inputData;
-}
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
-        return -1;
-    }
 
-    char *inputFN = argv[1];
+    char *data = "abc teakettleeeeeeee ijklM";
+    //char *data = "abcdefxxfoobarrrghabcxdefxteakettleeeeexxxxijklmxxdef";
+    fprintf(stdout, "data:[%s]\n", data);
 
     hs_database_t *db = NULL;
     hs_compile_error_t *compile_err = NULL;
     const char *expr[] = {
-        "abc",
-        "def",
-        "foobar.*gh",
+        "abc",              /* 101 */
+        "def",              /* 102 */
+        "foobar.*gh",       /* 103 */
         "teakettle{4,10}",  /* 104 */
         "ijkl[mMn]",        /* 105 */
-        "104 & 105",    /* 1001 */
-        "104 & !105"    /* 1002 */
-    };
-    
+        "105 & !104"         /*  1001 */ /* 注意: id大的规则在前面, 在执行id大的规则的时候，保证id小的规则已经匹配完成 */
+    }; 
+
+    /* hit 101 & 104 & 105 */
     unsigned flags[] = {
         HS_FLAG_SINGLEMATCH,
         HS_FLAG_SINGLEMATCH,
         HS_FLAG_SINGLEMATCH,
-        HS_FLAG_SINGLEMATCH,      /* 104 */
-        HS_FLAG_SINGLEMATCH,      /* 105 */
-        HS_FLAG_COMBINATION | HS_FLAG_SINGLEMATCH,  /* 1001 */
-        HS_FLAG_COMBINATION | HS_FLAG_SINGLEMATCH   /* 1002 */
-    };
+        HS_FLAG_SINGLEMATCH,
+        HS_FLAG_SINGLEMATCH,
+        HS_FLAG_COMBINATION | HS_FLAG_SINGLEMATCH};
 
-    unsigned ids[] = {101, 102, 103, 104, 105, 1001, 1002};
+    unsigned ids[] = {101, 102, 103, 104, 105, 1001};
     hs_error_t err = hs_compile_multi(expr, flags, ids, 
-            sizeof(ids)/sizeof(unsigned) , 
-            HS_MODE_NOSTREAM,
+            sizeof(ids)/sizeof(unsigned), HS_MODE_NOSTREAM,
             NULL, &db, &compile_err);
-
-    /* Next, we read the input data file into a buffer. */
-    unsigned int length;
-    char *inputData = readInputData(inputFN, &length);
-    if (!inputData) {
-        hs_free_database(db);
-        return -1;
-    }
 
     hs_scratch_t *scratch = NULL;
     if (hs_alloc_scratch(db, &scratch) != HS_SUCCESS) {
         fprintf(stderr, "ERROR: Unable to allocate scratch space. Exiting.\n");
-        free(inputData);
         hs_free_database(db);
         return -1;
     }
 
-    printf("Scanning %u bytes with Hyperscan\n", length);
-
-    if (hs_scan(db, inputData, length, 0, scratch, on_match, expr) != HS_SUCCESS) {
+    if (hs_scan(db, data, strlen(data), 0, scratch, on_match, expr) != HS_SUCCESS) {
         fprintf(stderr, "ERROR: Unable to scan input buffer. Exiting.\n");
         hs_free_scratch(scratch);
-        free(inputData);
         hs_free_database(db);
         return -1;
     }
 
-    /* Scanning is complete, any matches have been handled, so now we just
-     * clean up and exit.
-     */
     hs_free_scratch(scratch);
-    free(inputData);
     hs_free_database(db);
+
     return 0;
 }
