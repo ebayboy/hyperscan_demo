924c7640b06858441397f650977327618eb0dbfe
fanpengfei <fanpengfei@jd.com>
2019-03-26 05:37:43 -0400

    fw

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 
-gcc -g -o simplegrep simplegrep.c ./lib/libhs.a -lstdc++ -lm -I./include
+gcc -g -o simplegrep simplegrep.c ./lib/libhs.a -lstdc++ -lm -I./hs
+
+gcc -g -o simplegrep_multi simplegrep_multi.c ./lib/libhs.a -lstdc++ -lm -I./hs
+
+g++ -g -o pcapscan  examples/pcapscan.cc ./lib/libhs.a -lm -I./hs -std=gnu++11 -lpcap
